2cb9b0d3167b9508d418fb0c3abaca25c1c057f1
Krasius <DarkRisingForce@Gmail.com>
2019-04-20 18:56:34 +0300

    Setup admin site, added Entry model, and migrated the databse

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,3 +12,16 @@ class Topic(models.Model):
     def __str__(self):
         """Retruns a string representation of the model"""
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about the topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text[:50] + '...'
