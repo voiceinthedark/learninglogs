f6091c75bca0141ff1bf06355d8191b0d619d4c7
Krasius <DarkRisingForce@Gmail.com>
2019-04-22 10:41:51 +0300

    Setting up the app views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """The homepage for learning_logs"""
+    return render(request, 'learning_logs/index.html')
