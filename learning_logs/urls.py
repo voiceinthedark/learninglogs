3cfe7e7b2bc0079d8762ebe247670eb9a0ea8765
Krasius <DarkRisingForce@Gmail.com>
2019-04-24 10:15:31 +0300

    Add Topics page, modify base page and add views for topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,9 +1,12 @@
 """Define Urls patterns for learning_logs"""
 
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    #Home page
+    # Home page
     url(r'^$', views.index, name='index'),
+    # show all topics
+    path('topics/', views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     """The homepage for learning_logs"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
