37ffdb771d45c40ab903a9903eabca9995bb6b04
Krasius <DarkRisingForce@Gmail.com>
2019-04-24 10:21:03 +0300

    modify url to point to our app namespace

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
