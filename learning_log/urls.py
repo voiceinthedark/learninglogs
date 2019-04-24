3f4f0592e06d80e1d223ed018a1dcbbe6c00a8bf
Krasius <DarkRisingForce@Gmail.com>
2019-04-24 12:22:34 +0300

    fix namespacing

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-app_name = 'learning_logs'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning_logs.urls', namespace='learning_logs')),
